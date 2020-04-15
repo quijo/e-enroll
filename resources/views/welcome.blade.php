df8a6ad1545dcf8a7bcafc6c5c1dd6c75ec7a761
quijo <rjohnquia@gmail.com>
2020-04-15 21:27:17 +0800

    github connting to heroku test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Github to Heroku deploment
                 </div>
 
                 <div class="links">
