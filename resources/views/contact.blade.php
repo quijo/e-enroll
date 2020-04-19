38c019a9c4881959630e21d2124e68389503caba
quijo <rjohnquia@gmail.com>
2020-04-19 15:32:53 +0800

    add contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,1 +1,47 @@
 @extends('template.app')
+
+@section('content')
+<div class="contact-container">
+    <div class="contact-header">
+        <h1>Contact Us</h1>
+        <p>
+            Nulla labore pariatur mollit in incididunt minim enim. 
+            Fugiat ullamco esse labore ut aute id. Officia dolore qui elit
+            excepteur velit minim dolor dolore incididunt voluptate Lorem ut minim nisi.
+        </p>
+    </div>
+    <div class="contact-form">
+        <form >
+            <div class="col-auto">
+                <label class="sr-only" for="inlineFormInputGroup">Username</label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                    <div class="input-group-text border-0 bg-black text-blue">NAME</div>
+                    </div>
+                    <input type="text" class="form-control border-0 " id="inlineFormInputGroup" placeholder="Name">
+                </div>
+                 <div class="input-group mb-2">
+                    <div class="input-group-prepend ">
+                    <div class="input-group-text border-0 bg-black text-blue">EMAIL</div>
+                    </div>
+                    <input type="text" class="form-control border-0" id="inlineFormInputGroup" placeholder="Email">
+                </div>
+                 <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                    <div class="input-group-text border-0 bg-black text-blue">SUBJECT</div>
+                    </div>
+                    <input type="text" class="form-control border-0" id="inlineFormInputGroup" placeholder="Subject">
+                </div>
+                <div class="form-group contact-textarea">
+                    <label for="exampleFormControlTextarea1" class="bg-black text-blue textarea-label contact-textarea">Message</label>
+                    <textarea class="form-control border-0 " id="exampleFormControlTextarea1" rows="10"></textarea>
+                </div>
+                <button class="contact-btn bg-black text-blue border-0">Send</button>
+            </div>
+            
+        </form>
+    </div>
+
+</div>
+    
+@endsection
