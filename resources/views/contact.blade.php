b5dd7bba7f880d5e4475a1f52dbe5b26ce5e83b8
quijo <rjohnquia@gmail.com>
2020-04-19 16:34:58 +0800

    modify contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,27 +16,27 @@
                 <label class="sr-only" for="inlineFormInputGroup">Username</label>
                 <div class="input-group mb-2">
                     <div class="input-group-prepend">
-                    <div class="input-group-text border-0 bg-black text-blue">NAME</div>
+                    <div class="input-group-text border-0 bg-black text-blue contact-input">NAME</div>
                     </div>
                     <input type="text" class="form-control border-0 " id="inlineFormInputGroup" placeholder="Name">
                 </div>
                  <div class="input-group mb-2">
                     <div class="input-group-prepend ">
-                    <div class="input-group-text border-0 bg-black text-blue">EMAIL</div>
+                    <div class="input-group-text border-0 bg-black text-blue contact-input">EMAIL</div>
                     </div>
                     <input type="text" class="form-control border-0" id="inlineFormInputGroup" placeholder="Email">
                 </div>
                  <div class="input-group mb-2">
-                    <div class="input-group-prepend">
-                    <div class="input-group-text border-0 bg-black text-blue">SUBJECT</div>
+                    <div class="input-group-prepend ">
+                    <div class="input-group-text border-0 bg-black text-blue subject-prefext  ">SUBJECT</div>
                     </div>
                     <input type="text" class="form-control border-0" id="inlineFormInputGroup" placeholder="Subject">
                 </div>
-                <div class="form-group contact-textarea">
-                    <label for="exampleFormControlTextarea1" class="bg-black text-blue textarea-label contact-textarea">Message</label>
-                    <textarea class="form-control border-0 " id="exampleFormControlTextarea1" rows="10"></textarea>
+                <div class="form-group contact-textarea mb2 '">
+                    <label for="exampleFormControlTextarea1" class="bg-black text-blue textarea-label contact-textarea">MESSAGE</label>
+                    <textarea class="form-control border-0 message" id="exampleFormControlTextarea1" rows="10" ></textarea>
                 </div>
-                <button class="contact-btn bg-black text-blue border-0">Send</button>
+                <button class="contact-btn bg-black text-blue border-0">SEND</button>
             </div>
             
         </form>
