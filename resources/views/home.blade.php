2351a282f2939251e17a087c415e4d4c0d2e6876
quijo <rjohnquia@gmail.com>
2020-04-24 21:25:51 +0800

    edit navbar hover borderline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
+  <link rel="stylesheet" href="css/app.css">
   <link rel="stylesheet" href="css/style.css">
 
 </head>
