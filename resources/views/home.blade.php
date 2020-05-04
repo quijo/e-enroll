fd3d6f4e1d3229b0d38971fde214deaf53c32cb7
quijo <rjohnquia@gmail.com>
2020-05-04 22:02:53 +0800

    check var

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -105,7 +105,7 @@
 <script>
   //by chartjs.org
   var ctx = document.getElementById('myChart').getContext('2d');
-  var studentDb = [{{$prep}},{{$g1}},{{$g2}},{{$g3}},{{$g4}}, {{$g5}},{{$g6}},{{$g7}},{{$g8}},{{$g9}},{{$g10}},{{$g11}},{{$g12}}];
+  var studentDb = [{{$reserved_students}},{{$g1}},{{$g2}},{{$g3}},{{$g4}}, {{$g5}},{{$g6}},{{$g7}},{{$g8}},{{$g9}},{{$g10}},{{$g11}},{{$g12}}];
   var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
