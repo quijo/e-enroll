65f8c18faba39d802d163bb397b46e40547d3de8
quijo <rjohnquia@gmail.com>
2020-05-04 12:04:43 +0800

    changing variable name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,38 +26,38 @@ class HomeController extends Controller
     public function index()
     {
 
-     $preschool = Student::where('Grade_Level','Preschool')->where('status','approved')->get()->count();
-        $grade1 = Student::where('Grade_Level','Grade 1')->where('status','approved')->get()->count();
-        $grade2 = Student::where('Grade_Level','Grade 2')->where('status','approved')->get()->count();
-        $grade3 = Student::where('Grade_Level','Grade 3')->where('status','approved')->get()->count();
-        $grade4 = Student::where('Grade_Level','Grade 4')->where('status','approved')->get()->count();
-        $grade5 = Student::where('Grade_Level','grade 5')->where('status','approved')->get()->count();
-        $grade6 = Student::where('Grade_Level','grade 6')->where('status','approved')->get()->count();
-        $grade7 = Student::where('Grade_Level','grade 7')->where('status','approved')->get()->count();
-        $grade8 = Student::where('Grade_Level','grade 8')->where('status','approved')->get()->count();
-        $grade9 = Student::where('Grade_Level','grade 9')->where('status','approved')->get()->count();
-        $grade10 = Student::where('Grade_Level','grade 10')->where('status','approved')->get()->count();
-        $grade11 = Student::where('Grade_Level','grade 11')->where('status','approved')->get()->count();
-        $grade12 = Student::where('Grade_Level','grade 12')->where('status','approved')->get()->count();
+     $preschool_data = Student::where('Grade_Level','Preschool')->where('status','approved')->get()->count();
+        $grade1_data = Student::where('Grade_Level','Grade 1')->where('status','approved')->get()->count();
+        $grade2_data = Student::where('Grade_Level','Grade 2')->where('status','approved')->get()->count();
+        $grade3_data = Student::where('Grade_Level','Grade 3')->where('status','approved')->get()->count();
+        $grade4_data = Student::where('Grade_Level','Grade 4')->where('status','approved')->get()->count();
+        $grade5_data = Student::where('Grade_Level','grade 5')->where('status','approved')->get()->count();
+        $grade6_data = Student::where('Grade_Level','grade 6')->where('status','approved')->get()->count();
+        $grade7_data = Student::where('Grade_Level','grade 7')->where('status','approved')->get()->count();
+        $grade8_data = Student::where('Grade_Level','grade 8')->where('status','approved')->get()->count();
+        $grade9_data = Student::where('Grade_Level','grade 9')->where('status','approved')->get()->count();
+        $grade10_data = Student::where('Grade_Level','grade 10')->where('status','approved')->get()->count();
+        $grade11_data = Student::where('Grade_Level','grade 11')->where('status','approved')->get()->count();
+        $grade12_data = Student::where('Grade_Level','grade 12')->where('status','approved')->get()->count();
         $reserved_counts = Student::where('status', 'approved')->get()->count();
 
 
 
         //return the counting of approved studens plus per grade level count
         return view('home')
-        ->with('preschool',$preschool)
-        ->with('grade1',$grade1)
-        ->with('grade2',$grade2)
-        ->with('grade3',$grade3)
-        ->with('grade4',$grade4)
-        ->with('grade5',$grade5)
-        ->with('grade6',$grade6)
-        ->with('grade7',$grade7)
-        ->with('grade8',$grade8)
-        ->with('grade9',$grade9)
-        ->with('grade10',$grade10)
-        ->with('grade11',$grade11)
-        ->with('grade12',$grade12)
+        ->with('preschool',$preschool_data)
+        ->with('grade1',$grade1_data)
+        ->with('grade2',$grade2_data)
+        ->with('grade3',$grade3_data)
+        ->with('grade4',$grade4_data)
+        ->with('grade5',$grade5_data)
+        ->with('grade6',$grade6_data)
+        ->with('grade7',$grade7_data)
+        ->with('grade8',$grade8_data)
+        ->with('grade9',$grade9_data)
+        ->with('grade10',$grade10_data)
+        ->with('grade11',$grade11_data)
+        ->with('grade12',$grade12_data)
         ->with('counts',$reserved_counts);
 
 
